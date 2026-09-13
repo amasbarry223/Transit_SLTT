c60790a337a0ec92a8807f6d31a9e179700b0dad
Artifique <fombadaouda72@gmail.com>
2026-09-13 12:30:55 +0000

    chore: suppression des reliquats supabase et nettoyage du depot

--- supabase/migrations/20260710_enable_realtime_dashboard.sql
+++
@@ -1,9 +0,0 @@
--- Enable Supabase Realtime for dashboard-critical tables
-ALTER PUBLICATION supabase_realtime ADD TABLE dossiers;
-ALTER PUBLICATION supabase_realtime ADD TABLE ecritures;
-ALTER PUBLICATION supabase_realtime ADD TABLE factures;
-ALTER PUBLICATION supabase_realtime ADD TABLE clients;
-ALTER PUBLICATION supabase_realtime ADD TABLE stock_items;
-ALTER PUBLICATION supabase_realtime ADD TABLE bons_sortie;
-ALTER PUBLICATION supabase_realtime ADD TABLE devis;
-ALTER PUBLICATION supabase_realtime ADD TABLE profiles;
--- supabase/migrations/20260730_rename_societe_sltt.sql
+++
@@ -1,4 +0,0 @@
--- Bi-société : libellé client « SLTT » pour la société transit (retour client V1).
-UPDATE public.societes
-SET nom = 'SLTT'
-WHERE id = '22222222-2222-2222-2222-222222222222';
