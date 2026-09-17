131c2f4a034de50a543058f5715abb38d4d69bb8
Artifique <fombadaouda72@gmail.com>
2026-09-17 18:57:18 +0000

    chore(deploy): ajout du script start racine pour le déploiement Hostinger

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Point d'entrée racine pour Hostinger Node.js (si Application Root = public_html)
 const path = require('path');
 
-// Basculer dans le répertoire backend pour la résolution des fichiers et .env
-process.chdir(path.join(__dirname, 'backend'));
+const backendDir = path.join(__dirname, 'backend');
+process.chdir(backendDir);
 
-require('./backend/dist/main.js');
+require(path.join(backendDir, 'dist', 'main.js'));
